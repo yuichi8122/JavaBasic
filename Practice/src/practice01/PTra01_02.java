f3a64ad009ff3602899a52b0d03d2bf5a37a1973
PT55258HBMK\祐一 <yuichi.masuda.likeit@gmail.com>
2018-10-01 17:58:57 +0900

    １－４

--- Practice/src/practice01/PTra01_02.java
+++ Practice/src/practice01/PTra01_02.java
@@ -10,12 +10,11 @@ public class PTra01_02 {
 	public static void main(String[] args) {
 
 		// ★ 12～14行目をコメントにしてください。
-		ここをコメントにしてください
-		ここをコメントにしてください
-		ここをコメントにしてください
-
+		/*複数行の
+		 * コメントを
+		実施。 */
 		//  ★「Hello, world」と出力（コマンドプロンプトに表示）してください
 
-
+			System.out.println("Hello, world");
 	}
 }
