710f29be053f3688b0a1364e527b5c0670983118
PT55258HBMK\祐一 <yuichi.masuda.likeit@gmail.com>
2018-10-10 16:51:47 +0900

    7-2

--- Practice/src/practice07/PTra07_02.java
+++ Practice/src/practice07/PTra07_02.java
@@ -10,12 +10,23 @@ public class PTra07_02 {
 	public static void main(String[] args) {
 
 		// ★ int型の配列arrayを宣言し、1000個の領域で確保してください
-
+		int []array= new int [1000];
 
 		// ★ 配列arrayに [1,2,3,4,5,6,7,・・・・,998,999,1000] を代入してください
+		for (int i = 0; i < array.length; i++) {
+			array[i] = i+1;
+		}
+
+			for (int j : array) {
+			System.out.println(j);
+			}
+
+
 
 
 		// ★ 変数arrayに格納されている値を前から順番に全て表示してください
+
+
 
 	}
 }
