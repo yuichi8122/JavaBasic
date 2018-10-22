7689481431776674f7f0a7a89a66873209ffc64f
PT55258HBMK\祐一 <yuichi.masuda.likeit@gmail.com>
2018-10-22 16:27:30 +0900

    12-1

--- Practice/src/practice12/common/Student.java
+++ Practice/src/practice12/common/Student.java
@@ -13,20 +13,62 @@ package practice12.common;
  */
 public class Student {
 	/** 受講生ID */
-	public int studentId;
+	private int studentId;
 
 	/** 受講生氏名 */
-	public String studentName;
+	private String studentName;
 
 	/** 会社名 */
-	public String companyName;
+	private String companyName;
 
 	/** 教室名 */
-	public String className;
+	private String className;
 
 	/** メールアドレス */
-	public String mail;
+	private String mail;
 
 	/** パスワード */
-	public String password;
+	private String password;
+
+
+	public int getStudentId() {
+		return studentId;
+	}
+	public void setStudentId(int studentId) {
+		this.studentId=studentId;
+	}
+
+	public String getStudentName() {
+		return studentName;
+	}
+	public void setStudentName(String studentName) {
+		this.studentName=studentName;
+	}
+
+	public String getCompanyName() {
+		return companyName;
+	}
+	public void setCompanyName(String companyName) {
+		this.companyName=companyName;
+	}
+
+	public String getClassName() {
+		return className;
+	}
+	public void setClassName(String className) {
+		this.className=className;
+	}
+	public String getMail() {
+		return mail;
+	}
+	public void setMail(String mail) {
+		this.mail=mail;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password=password;
+	}
 }
